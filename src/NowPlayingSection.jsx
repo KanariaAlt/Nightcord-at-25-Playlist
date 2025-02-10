0ab410a7cdb001ed013e96f217fed7570d11d11c
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-02-10 08:41:29 +0000

    miku: cleanup lyrics

--- src/NowPlayingSection.jsx
+++ src/NowPlayingSection.jsx
@@ -1,6 +1,5 @@
 import './NowPlayingSection.scss';
 import { VideoPlayer } from './Player.jsx';
-import { Lyrics } from './Lyrics.jsx';
 import { Controller } from './Controller.jsx';
 
 import { MdKeyboardArrowUp } from 'react-icons/md';
